e412fcb47ff8cfc4d01e4bb00cc3403f1ea3e52d
Bubba Gump <[reid011@gannon.edu]>
2020-01-29 17:19:05 -0500

    Completed deck class structure

--- src/blackjack_02/Deck.java
+++ src/blackjack_02/Deck.java
@@ -11,4 +11,26 @@ package blackjack_02;
  */
 public class Deck {
     
+    private Card[] myCards = new Card[52];
+    private int nextCard = 0;
+    private String[] ranks = {};
+    private String[] suits = {};
+    
+    public Deck(){
+        initDeck();
+        shuffle();
+    }
+
+    private void initDeck() {
+       
+    }
+
+    private void shuffle() {
+       
+    }
+    
+    public Card dealCard(){
+        
+        return null; //Don't forgeet to change this!!!!!!!!!!!!!!!!!!!!!
+    }
 }
